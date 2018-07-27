fe24ef4a3cb3ae1a6f1c40689dbd664d6a197a7c
Jaremy Creechley <creechley@gmail.com>
2018-07-26 23:36:21 -0600

    Tweaking api

--- em_buffer.h
+++ em_buffer.h
@@ -25,24 +25,24 @@ struct byte_buff {
 
 typedef struct byte_buff buffer_t;
 
-void buffer_init(buffer_t* sb, char* data_buffer, em_size_t data_len);
+void buffer_init(buffer_t* data, char* data_buffer, em_size_t data_len);
 
-int buffer_available(buffer_t* sb);
+int buffer_available(buffer_t* data);
 
-int buffer_read_bytes(buffer_t* sb, uint8_t* buffer, int length);
+int buffer_read_bytes(buffer_t* data, em_byte_t* buffer, int length);
 
-uint16_t buffer_read_byte(buffer_t* sb);
+uint16_t buffer_read_byte(buffer_t* data);
 
-em_size_t buffer_write_bytes(buffer_t* sb, uint8_t* buffer, uint32_t data_len);
+em_size_t buffer_write_bytes(buffer_t* data, em_byte_t* buffer, uint32_t data_len);
 
-int buffer_peek(buffer_t* sb);
+int buffer_peek(buffer_t* data);
 
-void buffer_flush(buffer_t* sb);
+void buffer_flush(buffer_t* data);
 
-void buffer_clear(buffer_t* sb);
+void buffer_clear(buffer_t* data);
 
-void buffer_reset(buffer_t* sb);
+void buffer_reset(buffer_t* data);
 
-void buffer_reset_all(buffer_t* sb);
+void buffer_reset_all(buffer_t* data);
 
 #endif
